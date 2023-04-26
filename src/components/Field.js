3b14892e51d42aee8e30c7086e5847f3c2a1b239
eugene <eugenemugiraneza2@gmail.com>
2023-04-26 17:06:56 +0200

    my

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,14 +1,21 @@
 import React ,{useState}from 'react'
 
 const Field = () => {
+    const [value ,setValue]=useState('')
+const submitHandle=(e)=>{
+  e.preventDefault()
+}
+     
   return (
     <div className='filed'>
       <h1>Translate App</h1>
    <label>Enter English</label>
+   <form onSubmit={submitHandle}>
 <input
 className='input'
 value={value}
-onChange={((e)=>onChange(e.target.value))}/>
+onChange={((e)=>setValue(e.target.value))}/>
+</form>
     </div>
   )
 }
